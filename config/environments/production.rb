59e08b94d3c73cea9766d0239ea4bb03b39c0a2a
saadghouri75 <saad_ghouri75@yahoo.com>
2015-04-07 04:59:53 -0400

    changes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,4 +91,14 @@ Rails.application.configure do
   #   :enable_starttls_auto => true  }
   #   config.action_mailer.perform_deliveries = true
   config.action_mailer.default_url_options = { :host => "https://pacific-forest-5840.herokuapp.com" }
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+     :address        => 'smtp.gmail.com',
+     :port           => 587,
+     :domain         => 'gmail.com',
+     :authentication => :login,
+     :user_name      => 'bugticides@gmail.com',
+     :password       => 'bugticides12'
+   }
 end
